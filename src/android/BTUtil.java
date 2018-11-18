e5f4af0b44583ff832a678983cf84d8acd526bf1
Ferdynans Antoniush <ferdynans.web@gmail.com>
2018-11-18 14:08:03 +0700

    Add files via upload

---
+++ src/android/BTUtil.java
@@ -0,0 +1,17 @@
+package com.easy.btprinter;
+
+import android.content.Context;
+
+public class BTUtil {
+  public static int getResource(Context context, String resourceName, String resourceType) {
+    return context.getResources().getIdentifier(resourceName, resourceType, context.getPackageName());
+  }
+
+  public static int getStringResource(Context context, String resourceName) {
+    return BTUtil.getResource(context, resourceName, "string");
+  }
+
+  public static String getStringFromStringResource(Context context, String resourceName) {
+    return context.getString(BTUtil.getStringResource(context, resourceName));
+  }
+}
